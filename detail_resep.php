595ac6d91814841f36680fe778d6aef8aa5da037
Kevin Adisurya Nugraha <habeelkevin@gmail.com>
2024-10-16 11:46:51 +0700

    Memindahkan kode css ke file css

--- detail_resep.php
+++ detail_resep.php
@@ -25,54 +25,7 @@ if (!$resep) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail Resep Makanan</title>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        body {
-            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-            background-color: #FFECC8;
-        }
-
-        .container .jumbotron {
-            max-width: 900px;
-            margin: auto;
-            padding-bottom: 3rem;
-        }
-
-        .container .jumbotron h1 {
-            color: #333;
-            text-align: center;
-            padding: 2rem 0 1rem 0;
-        }
-
-        .container .jumbotron .card .isi {
-            margin-top: 2rem;
-        }
-
-        .container .jumbotron .card .tombol {
-            margin-top: 2rem;
-        }
-
-        .container .jumbotron .card .tombol a {
-            background-color: #333;
-            text-decoration: none;
-            color: #fff;
-            padding: 0.5rem 1rem;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: 0.3s ease-in-out;
-        }
-
-        .container .jumbotron .card .tombol a:hover {
-            text-decoration: none;
-            background-color: #666;
-        }
-    </style>
+    <link rel="stylesheet" href="css/detail.css">
 </head>
 
 <body>
